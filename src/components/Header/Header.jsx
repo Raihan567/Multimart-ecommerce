29c9c4c822cf910b8115a1f164d8455dfa30282c
Raihan567 <aburaihansarker567@gmail.com>
2022-10-24 21:22:00 +0600

    navbar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,64 +4,75 @@ import { Container, Row } from "reactstrap";
 import Logo from "../../assets/images/eco-logo.png";
 import userIcon from "../../assets/images/user-icon.png";
 import "./Header.css";
+import { motion } from "framer-motion";
+
+const nav__links = [
+  {
+    path: "home",
+    display: "Home",
+  },
+  {
+    path: "cart",
+    display: "Cart",
+  },
+  {
+    path: "shop",
+    display: "Shop",
+  },
+];
 const Header = () => {
   return (
     <header className="header">
       <Container>
         <Row>
           <div className="nav__wrapper">
+            {/* logo */}
             <div className="logo">
               <img src={Logo} alt="logo" />
               <div>
                 <h1>Multimart</h1>
-                <p>Since 1995</p>
+                {/* <span>Since 1995</span> */}
               </div>
             </div>
-            {/* Navigation */}
 
+            {/* Navigation */}
             <div className="navigation">
               <div className="nav__link">
                 <ul className="menu">
-                  <li>
-                    <NavLink className="text-decoration-none text-dark" to="/">
-                      Home
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="text-decoration-none text-dark"
-                      to="/shop"
-                    >
-                      Shop
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      className="text-decoration-none text-dark"
-                      to="/cart"
-                    >
-                      Cart
-                    </NavLink>
-                  </li>
+                  {nav__links.map((item, index) => (
+                    <li className="nav__item" key={index}>
+                      <NavLink
+                        to={item.path}
+                        className={(navClass) =>
+                          navClass.isActive ? "nav__active" : ""
+                        }
+                      >
+                        {item.display}
+                      </NavLink>
+                    </li>
+                  ))}
                 </ul>
               </div>
             </div>
+
             {/* Nav Icon */}
             <div className="nav__icon">
               <span className="fav__icon">
-                <i class="ri-heart-line"></i>
+                <i className="ri-heart-line"></i>
+                <span className="badge">0</span>
               </span>
               <span className="cart__icon">
-                <i class="ri-shopping-bag-line"></i>
+                <i className="ri-shopping-bag-line"></i>
+                <span className="badge">10</span>
               </span>
               <span>
-                <img src={userIcon} alt="" />
+                <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" />
               </span>
             </div>
             {/* Mobile menu */}
             <div className="mobile__menu">
               <span className="">
-                <i class="ri-menu-line"></i>
+                <i className="ri-menu-line"></i>
               </span>
             </div>
           </div>
