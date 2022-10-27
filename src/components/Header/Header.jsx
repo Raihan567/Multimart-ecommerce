ecb481d956b632c83b982594f2c87e37c9b28b7b
Raihan567 <aburaihansarker567@gmail.com>
2022-10-28 04:25:11 +0600

    implement search and filter function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import { motion } from "framer-motion";
+import React, { useRef } from "react";
+import { useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 import { Container, Row } from "reactstrap";
 import Logo from "../../assets/images/eco-logo.png";
 import userIcon from "../../assets/images/user-icon.png";
 import "./Header.css";
-import { motion } from "framer-motion";
 
 const nav__links = [
   {
@@ -26,6 +27,7 @@ const nav__links = [
 ];
 const Header = () => {
   // const headerRef = useRef(null);
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity);
   const menuRef = useRef(null);
   // const stickyHeaderFunc = () => {
   //   window.addEventListener("scroll", () => {
@@ -87,7 +89,7 @@ const Header = () => {
               </span>
               <span className="cart__icon">
                 <i className="ri-shopping-bag-line"></i>
-                <span className="badge">10</span>
+                <span className="badge">{totalQuantity}</span>
               </span>
               <span>
                 <motion.img whileTap={{ scale: 1.2 }} src={userIcon} alt="" />
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,13 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
+import { Col, Container, Row } from "reactstrap";
+import products from "../assets/data/products";
 import Helmet from "../components/Helmet/Helmet";
-
+import CommonSection from "../components/Ui/CommonSection";
+import ProductList from "../components/Ui/ProductList";
+import "../styles/shop.css";
 const Shop = () => {
+  const [productsData, setProductsData] = useState(products);
+
+  const handleFilter = (e) => {
+    const filterValue = e.target.value;
+
+    if (filterValue === "chair") {
+      const filterProducts = products.filter(
+        (item) => item.category === "chair"
+      );
+
+      setProductsData(filterProducts);
+    }
+
+    if (filterValue === "mobile") {
+      const filterProducts = products.filter(
+        (item) => item.category === "mobile"
+      );
+
+      setProductsData(filterProducts);
+    }
+
+    if (filterValue === "sofa") {
+      const filterProducts = products.filter(
+        (item) => item.category === "sofa"
+      );
+
+      setProductsData(filterProducts);
+    }
+
+    if (filterValue === "wireless") {
+      const filterProducts = products.filter(
+        (item) => item.category === "wireless"
+      );
+
+      setProductsData(filterProducts);
+    }
+
+    if (filterValue === "watch") {
+      const filterProducts = products.filter(
+        (item) => item.category === "watch"
+      );
+
+      setProductsData(filterProducts);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const searchTerm = e.target.value;
+    const searchProduct = products.filter((item) =>
+      item.productName
+        .toLocaleLowerCase()
+        .includes(searchTerm.toLocaleLowerCase())
+    );
+
+    setProductsData(searchProduct);
+  };
+
   return (
     <Helmet title={"Shop"}>
-      <h2>Shop</h2>
+      <CommonSection title="Products" />
+
+      <Container>
+        <Row className="my-5">
+          <Col lg="3" md="3">
+            <div className="filter__widget">
+              <select onChange={handleFilter}>
+                <option>Filter By Category</option>
+                <option value="sofa">Sofa</option>
+                <option value="mobile">Mobile</option>
+                <option value="chair">Chair</option>
+                <option value="watch">Watch</option>
+                <option value="wireless">Wireless</option>
+              </select>
+            </div>
+          </Col>
+          <Col lg="3" md="3">
+            <div className="filter__widget">
+              <select>
+                <option disabled>Sort By Order</option>
+                <option value="ascending">Ascending</option>
+                <option value="descending">Descending</option>
+              </select>
+            </div>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="search__box">
+              <input
+                type="text"
+                placeholder="Search what you need..."
+                onChange={handleSearch}
+              />
+              <span>
+                <i className="ri-search-line "></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {productsData.length === 0 ? (
+              <h1 className="text-center fs-3">No Products are found!</h1>
+            ) : (
+              <ProductList data={productsData} />
+            )}
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   );
 };
 
 export default Shop;
- 
